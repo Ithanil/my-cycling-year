56d8c9942502a6dc1f260b6cedd29039cf2883d3
Jan Kessler <jakessle@t-online.de>
2021-12-30 20:35:14 +0100

    avoid requesting/storing already stored activities (especially to avoid hitting API limit)

--- store_activities_year_locally.py
+++ store_activities_year_locally.py
@@ -7,7 +7,21 @@ client = StravaIO(access_token=get_token.token['access_token'])
 athlete = client.get_logged_in_athlete()
 athlete.store_locally()
 
+# Generate list of local athletes
+local_athl_gen = client.local_athletes()
+local_athl_ids = []
+for athl in local_athl_gen:
+    local_athl_ids.append(athl['id'])
+
+# Generate list of local activities
+local_act_ids = []
+for athl_id in local_athl_ids:
+    local_acts_gen = client.local_activities(athl_id)
+    for act in local_acts_gen:
+        local_act_ids.append(act['id'])
+
 # Store Activities
 list_activities = client.get_logged_in_athlete_activities(after='last year')
 for a in list_activities:
-    client.get_activity_by_id(a.id).store_locally()
+    if a.id not in local_act_ids:
+        client.get_activity_by_id(a.id).store_locally()
